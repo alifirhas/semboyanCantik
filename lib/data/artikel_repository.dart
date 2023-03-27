2e06d6f1b300b76a88764b18aac647a665c63bb8
alifirhas <irhasalif@gmail.com>
2023-03-27 19:57:02 +0700

    :sparkles: Update artikel repository

--- lib/data/artikel_repository.dart
+++ lib/data/artikel_repository.dart
@@ -5,7 +5,7 @@ List<ArtikelModel> artikelData = [
   ArtikelModel(
     id: '1',
     title: 'Mengenali Gejala Stunting pada Balita yang Perlu Diperhatikan',
-    imgPath: 'lib/images/artikel/bayi.jpg',
+    imgPath: 'lib/images/artikel/artikel1.jpg',
     viewCount: '92',
     createdAt: DateTime(2023, 1, 18),
     content:
@@ -15,7 +15,7 @@ List<ArtikelModel> artikelData = [
   ArtikelModel(
     id: '2',
     title: 'Faktor Penyebab Stunting pada Balita dan Cara Mengatasinya',
-    imgPath: 'lib/images/artikel/bayi.jpg',
+    imgPath: 'lib/images/artikel/artikel2.jpg',
     viewCount: '39',
     createdAt: DateTime(2023, 2, 10),
     content:
@@ -35,7 +35,7 @@ List<ArtikelModel> artikelData = [
   ArtikelModel(
     id: '4',
     title: 'Mengoptimalkan Asupan Gizi Balita untuk Mencegah Stunting',
-    imgPath: 'lib/images/artikel/bayi.jpg',
+    imgPath: 'lib/images/artikel/artikel3.jpg',
     viewCount: '1',
     createdAt: DateTime(2023, 4, 03),
     content:
@@ -45,7 +45,7 @@ List<ArtikelModel> artikelData = [
   ArtikelModel(
     id: '5',
     title: 'Peran Penting Keluarga dalam Mencegah Stunting Balita',
-    imgPath: 'lib/images/artikel/bayi.jpg',
+    imgPath: 'lib/images/artikel/artikel5.jpg',
     viewCount: '84',
     createdAt: DateTime(2023, 5, 07),
     content:
@@ -57,7 +57,7 @@ List<ArtikelModel> artikelData = [
   ArtikelModel(
     id: '6',
     title: '5 Cara Mudah Mencegah Stunting pada Balita yang Perlu Anda Ketahui',
-    imgPath: 'lib/images/artikel/bayi.jpg',
+    imgPath: 'lib/images/artikel/artikel6.jpg',
     viewCount: '23',
     createdAt: DateTime(2023, 6, 11),
     content:
@@ -67,7 +67,7 @@ List<ArtikelModel> artikelData = [
   ArtikelModel(
     id: '7',
     title: 'Pentingnya Peran Orang Tua dalam Pencegahan Stunting pada Balita',
-    imgPath: 'lib/images/artikel/bayi.jpg',
+    imgPath: 'lib/images/artikel/artikel7.jpg',
     viewCount: '61',
     createdAt: DateTime(2023, 7, 08),
     content:
@@ -77,7 +77,7 @@ List<ArtikelModel> artikelData = [
   ArtikelModel(
     id: '8',
     title: 'Program Pemerintah untuk Mencegah Stunting pada Balita: Apa Saja?',
-    imgPath: 'lib/images/artikel/bayi.jpg',
+    imgPath: 'lib/images/artikel/artikel8.jpg',
     viewCount: '46',
     createdAt: DateTime(2023, 8, 25),
     content:
@@ -87,7 +87,7 @@ List<ArtikelModel> artikelData = [
   ArtikelModel(
     id: '9',
     title: '5 Cara Mudah untuk Mencegah Stunting pada Balita di Rumah',
-    imgPath: 'lib/images/artikel/bayi.jpg',
+    imgPath: 'lib/images/artikel/artikel9.jpg',
     viewCount: '88',
     createdAt: DateTime(2023, 9, 14),
     content:

703fbaf1698348f44888e92a4a2e305f854be647
alifirhas <irhasalif@gmail.com>
2023-03-27 22:07:18 +0700

    :construction: Child growth standard data

---
+++ lib/data/child_growth_standard_repository.dart
@@ -0,0 +1,42 @@
+import 'package:startertemplate/models/child_growth_standard_model.dart';
+
+List<Map<String, dynamic>> childGrowthStandard = [
+  {
+    'gender': 'boy',
+    'values': [
+      ChildGrowthStandardModel(
+        week: 0,
+        l: 0,
+        m: 0,
+        s: 0,
+        sd: 0,
+        sd3neg: 0,
+        sd2neg: 0,
+        sd1neg: 0,
+        sd0: 0,
+        sd1: 0,
+        sd2: 0,
+        sd3: 0,
+      )
+    ],
+  },
+  {
+    'gender': 'girl',
+    'values': [
+      ChildGrowthStandardModel(
+        week: 0,
+        l: 0,
+        m: 0,
+        s: 0,
+        sd: 0,
+        sd3neg: 0,
+        sd2neg: 0,
+        sd1neg: 0,
+        sd0: 0,
+        sd1: 0,
+        sd2: 0,
+        sd3: 0,
+      )
+    ],
+  },
+];
---
+++ lib/models/child_growth_standard_model.dart
@@ -0,0 +1,29 @@
+class ChildGrowthStandardModel {
+  final int week;
+  final double l;
+  final double m;
+  final double s;
+  final double sd;
+  final double sd3neg;
+  final double sd2neg;
+  final double sd1neg;
+  final double sd0;
+  final double sd1;
+  final double sd2;
+  final double sd3;
+
+  ChildGrowthStandardModel({
+    required this.week,
+    required this.l,
+    required this.m,
+    required this.s,
+    required this.sd,
+    required this.sd3neg,
+    required this.sd2neg,
+    required this.sd1neg,
+    required this.sd0,
+    required this.sd1,
+    required this.sd2,
+    required this.sd3,
+  });
+}
